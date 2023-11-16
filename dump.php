f54a27b72b0a6e85e4f6fa9e79d2e198c05fcce8
manonphi@hotmail.fr <manonphi@hotmail.fr>
2023-11-16 15:58:01 +0100

    Premier commit

---
+++ dump.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+    //initialisation de variables
+$number =123;
+$text ="foo bar baz";
+$flag =true;
+
+    //initialisation de variables
+var_dump($number);
+var_dump($text);
+var_dump($flag);
+
+    //initialisation de variables
+dump($number);
+dump($text);
+dump($flag);
+
